ffcbe553c5a8046f40a971ac7d987a8c3895ffb8
Allan Ninal <landix.ninal@gmail.com>
2023-07-24 07:39:27 +0800

    simple sample data

--- includes/Error_Logs_List_Table.php
+++ includes/Error_Logs_List_Table.php
@@ -151,7 +151,15 @@ class Error_Logs_List_Table extends WP_List_Table {
 
         $result = json_decode( $data, true );
 
-        return $result;
+        return array(
+            array(
+                'id' => 100,
+                'module' => "contacts",
+                'code' => 400,
+                'message' => "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.",
+                'log_time' => "2023-04-07 14:24:09"
+            )
+        );
 
     }
 
