e9977db89296c6b55ad3d580b8c9d5641b1416b4
Allan Ninal <landix.ninal@gmail.com>
2023-07-26 16:18:43 +0800

    added insert log function

--- includes/Bocs_Log_Handler.php
+++ includes/Bocs_Log_Handler.php
@@ -2,7 +2,83 @@
 
 class Bocs_Log_Handler {
     
-    public function insert_log(  ) {
+    /**
+     * Adds logs on the woocommerce_log table
+     * 
+     * @param string $level 
+     *
+     * The level numbers and their corresponding severity levels are as follows:
+     * 
+     * 0 - Emergency: The highest severity level, indicating a system-wide critical failure.
+     * 1 - Alert: An urgent situation that requires immediate attention.
+     * 2 - Critical: A critical condition that needs to be addressed promptly. 
+     * 3 - Error: An error has occurred, but the system can still function.
+     * 4 - Warning: An issue that may require attention but doesn’t disrupt the system.
+     * 5 - Notice: General information about the system’s operation.
+     * 6 - Informational: Additional information about system events.
+     * 7 - Debug: Detailed debugging information, useful for troubleshooting.
+     *
+     * @param string $message
+     * @param string $context
+     * 
+     * @return void
+     */
+    public function insert_log( $level = 'debug', $message, $context ) {
+
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "woocommerce_log";
+
+        $level_number = 7;
+
+        switch( $level ){
+
+            case 'emergency':
+                $level_number = 0;
+                break;
+            
+            case 'alert':
+                $level_number = 1;
+                break;
+            
+            case 'critical':
+                $level_number = 2;
+                break;
+            
+            case 'error':
+                $level_number = 3;
+                break;
+            
+            case 'warning':
+                $level_number = 4;
+                break;
+            
+            case 'notice':
+                $level_number = 5;
+                break;
+            
+            case 'info':
+            case 'information':
+                $level_number = 6;
+                break;
+            
+            case 'debug':
+
+            default:
+                $level_number = 7;
+                break;
+
+        }
+
+        return $wpdb->query(
+            $wpdb->prepare(
+                "INSERT INTO {$table_name} (timestamp, level, source, message, context) VALUES ( NOW(), %d, %s, %s, %s )",
+                $level_number,
+                'bocs',
+                $message,
+                $context
+            )
+        );
         
     }
 
