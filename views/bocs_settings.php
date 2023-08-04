24abb0f2ecfa4f1c210fbf67eb3e78247c7a7a5f
Allan Ninal <landix.ninal@gmail.com>
2023-08-04 11:22:34 +0800

    updated the page for the bocs settings

--- views/bocs_settings.php
+++ views/bocs_settings.php
@@ -1,24 +1,63 @@
+<?php
+
+$bocs = new Bocs();
+$bocs->auto_add_bocs_keys();
+
+$options = get_option( 'bocs_plugin_options' );
+$options['bocs_headers'] = $options['bocs_headers'] ?? array(); ?>
+
 <div class="tabset">
   <!-- Tab 1 -->
   <input type="radio" name="tabset" id="tab1" aria-controls="marzen" checked>
-  <label for="tab1">Märzen</label>
+  <label for="tab1">Credentials / Keys</label>
   <!-- Tab 2 -->
   <input type="radio" name="tabset" id="tab2" aria-controls="rauchbier">
-  <label for="tab2">Rauchbier</label>
+  <label for="tab2">Sync Logs</label>
   <!-- Tab 3 -->
   <input type="radio" name="tabset" id="tab3" aria-controls="dunkles">
   <label for="tab3">Dunkles Bock</label>
   
   <div class="tab-panels">
     <section id="marzen" class="tab-panel">
-      <h2>6A. Märzen</h2>
-      <p><strong>Overall Impression:</strong> An elegant, malty German amber lager with a clean, rich, toasty and bready malt flavor, restrained bitterness, and a dry finish that encourages another drink. The overall malt impression is soft, elegant, and complex, with a rich aftertaste that is never cloying or heavy.</p>
-      <p><strong>History:</strong> As the name suggests, brewed as a stronger “March beer” in March and lagered in cold caves over the summer. Modern versions trace back to the lager developed by Spaten in 1841, contemporaneous to the development of Vienna lager. However, the Märzen name is much older than 1841; the early ones were dark brown, and in Austria the name implied a strength band (14 °P) rather than a style. The German amber lager version (in the Viennese style of the time) was first served at Oktoberfest in 1872, a tradition that lasted until 1990 when the golden Festbier was adopted as the standard festival beer.</p>
+      <h2>Credentials / Keys</h2>
+      <form method="post">
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row" class="titledesc">
+						<label>Store ID</label>
+					</th>
+					<td class="forminp forminp-text">
+						<input type="text" name="bocs_plugin_options[bocs_headers][store]" id="bocsStore" value="<?php echo $options['bocs_headers']['store'] ?? "" ?>" />
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row" class="titledesc">
+						<label>Organization ID</label>
+					</th>
+					<td class="forminp forminp-text">
+						<input type="text" name="bocs_plugin_options[bocs_headers][organization]" id="bocsOrganization" value="<?php echo $options['bocs_headers']['organization'] ?? "" ?>" />
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row" class="titledesc">
+						<label>Authorization</label>
+					</th>
+					<td class="forminp forminp-text">
+						<input type="text" name="bocs_plugin_options[bocs_headers][authorization]" id="bocsAuthorization" value="<?php echo $options['bocs_headers']['authorization'] ?? "" ?>" />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<input type="hidden" id="_wpnonce" name="_wpnonce" value="<?php echo wp_create_nonce("bocs_plugin_options"); ?>">
+		<input type="hidden" name="action" value="update">
+		<input type="hidden" name="option_page" value="bocs_plugin_options">
+		<button type="submit" class="button-primary woocommerce-save-button">Submit</button>
+	</form>
   </section>
     <section id="rauchbier" class="tab-panel">
-      <h2>6B. Rauchbier</h2>
-      <p><strong>Overall Impression:</strong>  An elegant, malty German amber lager with a balanced, complementary beechwood smoke character. Toasty-rich malt in aroma and flavor, restrained bitterness, low to high smoke flavor, clean fermentation profile, and an attenuated finish are characteristic.</p>
-      <p><strong>History:</strong> A historical specialty of the city of Bamberg, in the Franconian region of Bavaria in Germany. Beechwood-smoked malt is used to make a Märzen-style amber lager. The smoke character of the malt varies by maltster; some breweries produce their own smoked malt (rauchmalz).</p>
+      <h2>Sync Logs</h2>
     </section>
     <section id="dunkles" class="tab-panel">
       <h2>6C. Dunkles Bock</h2>
