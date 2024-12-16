6f9717279cc07ef7d1ec45c767aaebbd526c5c2b
Allan Ninal <landix.ninal@gmail.com>
2024-12-16 17:33:50 +0800

    chore: use === and ==

--- includes/Admin.php
+++ includes/Admin.php
@@ -490,7 +490,7 @@ class Admin
 
             wp_enqueue_script('bocs-cart-js', plugin_dir_url(__FILE__) . '../assets/js/bocs-cart.js', array(
                 'jquery'
-            ), '20240705.1', true);
+            ), '20241216.0', true);
 
             wp_localize_script('bocs-cart-js', 'bocsCartObject', array(
                 'ajax_url' => admin_url('admin-ajax.php'),
