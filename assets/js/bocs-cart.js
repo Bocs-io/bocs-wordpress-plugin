ff3bfa9102867b5c789951cec2c0d66b81d9d4fa
Allan Ninal <landix.ninal@gmail.com>
2024-12-16 17:29:19 +0800

    chore: use === and ==

--- assets/js/bocs-cart.js
+++ assets/js/bocs-cart.js
@@ -85,13 +85,13 @@ function formatFrequency(frequency, timeUnit) {
 function getOrdinalSuffix(number) {
     const j = number % 10,
           k = number % 100;
-    if (j == 1 && k != 11) {
+    if (j === 1 && k !== 11) {
         return number + "st";
     }
-    if (j == 2 && k != 12) {
+    if (j === 2 && k !== 12) {
         return number + "nd";
     }
-    if (j == 3 && k != 13) {
+    if (j === 3 && k !== 13) {
         return number + "rd";
     }
     return number + "th";
